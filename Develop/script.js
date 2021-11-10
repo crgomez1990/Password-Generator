7905b6172eb3af2286d94fa6ffe6a9af28253202
crgomez1990 <crgomez1990@gmail.com>
2021-11-09 18:16:07 -0600

    Added Javascript to handle password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var numChar = prompt("How long do you want your password to be?")
+  numChar = parseInt(numChar)
+  while (numChar < 8 || numChar > 128){
+    alert("Password length needs to be at least 8 and no more than 128 characters")
+    numChar = prompt("How long do you want your password to be?")
+    numChar = parseInt(numChar)
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
